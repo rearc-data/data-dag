428f778ed671cad7363273097db3a8e5dede4742
David Maxson <david.maxson@rearc.io>
2022-07-12 09:42:23 -0700

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, David Maxson (david.maxson@rearc.io)"
 author = "David Maxson (david.maxson@rearc.io)"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 
